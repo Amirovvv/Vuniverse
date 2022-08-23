7d39521f9ae0aeb21fbef8509eccacf0bdaa2e98
Khasan <a.amirovv@mail.ru>
2022-08-23 22:45:51 +0300

    test linter

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -3,41 +3,41 @@ import { ref } from 'vue'
 import { Section } from '@/models/sections.models'
 
 const sections = ref<Section[]>([
-	{
-		title: 'Тесты',
-		items: [{ title: 'Тест на знание основ Vue JS', url: '/tests' }],
-	},
-	{
-		title: 'Полезные ссылки',
-		items: [
-			{ title: '--', url: '--' },
-			{ title: '--', url: '--' },
-		],
-	},
+  {
+    title: 'Тесты',
+    items: [{ title: 'Тест на знание основ Vue JS', url: '/tests' }],
+  },
+  {
+    title: 'Полезные ссылки',
+    items: [
+      { title: '--', url: '--' },
+      { title: '--', url: '--' },
+    ],
+  },
 ])
 </script>
 
 <template>
-	<aside :class="$style.sidebar">
-		<nav class="nav">
-			<section
-				:class="$style.section"
-				v-for="(section, index) in sections"
-				:key="`section-${index}`"
-			>
-				<div :class="$style.sectionTitle">{{ section.title }}</div>
-				<div
-					:class="$style.sectionLink"
-					v-for="(item, index) in section.items"
-					:key="`item-${index}`"
-				>
-					<router-link :to="item.url" :active-class="$style.sectionLinkActive">
-						{{ item.title }}
-					</router-link>
-				</div>
-			</section>
-		</nav>
-	</aside>
+  <aside :class="$style.sidebar">
+    <nav class="nav">
+      <section
+        :class="$style.section"
+        v-for="(section, index) in sections"
+        :key="`section-${index}`"
+      >
+        <div :class="$style.sectionTitle">{{ section.title }}</div>
+        <div
+          :class="$style.sectionLink"
+          v-for="(item, index) in section.items"
+          :key="`item-${index}`"
+        >
+          <router-link :to="item.url" :active-class="$style.sectionLinkActive">
+            {{ item.title }}
+          </router-link>
+        </div>
+      </section>
+    </nav>
+  </aside>
 </template>
 
 <style module>
